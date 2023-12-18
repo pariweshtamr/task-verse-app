4c98e718b92f605256202fc77522a351aa498ece
pariwesh <tamrpariwesh@gmail.com>
2023-12-19 07:33:04 +1100

    ignore ts check in db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -5,8 +5,10 @@ declare global {
   let prisma: PrismaClient | undefined
 }
 
+// @ts-ignore
 const db = globalThis.prisma ?? new PrismaClient()
 
+// @ts-ignore
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db
 
 export default db
