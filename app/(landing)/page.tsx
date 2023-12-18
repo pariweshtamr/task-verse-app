cc5c470981d11467df69a31c61bc53f9f2b72ddf
pariwesh <tamrpariwesh@gmail.com>
2023-12-18 22:15:30 +1100

    minor ui change to landing page and navbar

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -18,11 +18,11 @@ const LandingPage = () => {
     <div className="flex items-center justify-center flex-col">
       <div
         className={cn(
-          "flex items-center justify-center flex-col",
+          "flex items-center justify-center flex-col mb-8",
           headingFont.className
         )}
       >
-        <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
+        <div className="mb-8 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
           <Medal className="h-6 w-6 mr-2" />
           No 1 task management
         </div>
