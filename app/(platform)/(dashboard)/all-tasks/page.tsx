3a93c48c948241c484221ceb8fc8552e06f41de0
pariwesh <tamrpariwesh@gmail.com>
2023-12-19 19:58:17 +1100

    bug fix for no option to create task for the very first time in mobile version

--- app/(platform)/(dashboard)/all-tasks/page.tsx
+++ app/(platform)/(dashboard)/all-tasks/page.tsx
@@ -20,15 +20,9 @@ const AllTasksPage = async () => {
   })
   return (
     <div className="mt-4">
-      {tasks?.length === 0 ? (
-        <h3 className="text-txtColor text-xl">
-          You have not created any tasks!
-        </h3>
-      ) : (
-        <Suspense fallback={<TaskList.Skeleton />}>
-          <TaskList allTasks={tasks} />
-        </Suspense>
-      )}
+      <Suspense fallback={<TaskList.Skeleton />}>
+        <TaskList allTasks={tasks} />
+      </Suspense>
     </div>
   )
 }
